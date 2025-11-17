35800aaf35f6b3cc9f2549a2042cf5ef9c9dcbc1
Brian Bischoff <bbischoff78@gmail.com>
2025-11-17 15:32:54 -0500

    modified to use f3rva.org as the base and use the workout date as the URL to lookup, not the post date.

--- rss-enhanced.php
+++ rss-enhanced.php
@@ -9,6 +9,7 @@ $posts = query_posts('showposts=' . $postCount);
 header('Content-Type: '.feed_content_type('rss-http').'; charset='.get_option('blog_charset'), true);
 echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?'.'>';
 
+$new_domain = 'https://f3rva.org';
 ?>
 
 <rss version="2.0"
@@ -22,7 +23,7 @@ echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?'.'>';
 <channel>
         <title><?php bloginfo_rss('name'); ?></title>
         <atom:link href="<?php self_link(); ?>" rel="self" type="application/rss+xml" />
-        <link><?php bloginfo_rss('url') ?></link>
+        <link><?php echo $new_domain ?></link>
         <description><?php bloginfo_rss('description') ?></description>
         <lastBuildDate><?php echo mysql2date('D, d M Y H:i:s +0000', get_lastpostmodified('GMT'), false); ?></lastBuildDate>
         <language><?php echo get_option('rss_language'); ?></language>
@@ -45,12 +46,39 @@ echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?'.'>';
 	
 	echo $content;
                         ?></title>
-                        <link><?php the_permalink_rss(); ?></link>
+                        <link><?php
+    $workout_date = get_field('workout_date_new', get_the_ID());
+	$workout_url = '';
+    
+    if ($workout_date) {
+        // Create a DateTime object from the ACF field value.
+        // Assumes the date is stored in a format PHP can understand (like Y-m-d).
+        $date_obj = date_create($workout_date);
+        if ($date_obj) {
+            $date_path = date_format($date_obj, 'Y/m/d');
+            $slug = get_post_field('post_name', get_the_ID());
+			$workout_url = $new_domain . '/' . $date_path . '/' . $slug . '/';
+            echo esc_url($workout_url);
+        } 
+    } else {
+        echo $new_domain;
+    }
+?></link>
                         <pubDate><?php echo mysql2date('D, d M Y H:i:s +0000', get_post_time('Y-m-d H:i:s', true), false); ?></pubDate>
                         <dc:creator><?php the_author(); ?></dc:creator>
                         <guid isPermaLink="false"><?php the_guid(); ?></guid>
-                        <description><![CDATA[<?php the_excerpt_rss() ?>]]></description>
-                        <content:encoded><![CDATA[<?php the_excerpt_rss() ?>]]></content:encoded>
+                        <description><![CDATA[<?php
+    $site_url = get_site_url();
+
+	// This regex finds URLs in href attributes that start with the site URL
+	$pattern = '/(href=["\'])' . preg_quote($site_url, '/') . '[^"\']*(["\'])/';
+
+	// This will replace the entire old URL with the new one
+	$replacement = '$1' . $workout_url . '$2';
+
+	$excerpt_replaced = preg_replace($pattern, $replacement, get_the_excerpt());
+	echo apply_filters( 'the_excerpt_rss', $excerpt_replaced );
+?>]]></description>
                         <?php rss_enclosure(); ?>
                         <?php do_action('rss2_item'); ?>
                 </item>
